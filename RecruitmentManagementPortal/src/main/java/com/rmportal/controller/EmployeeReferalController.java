b2a7b37da21469fee1c9f054be25b67e79d0588c
Sourabh J <jsourabh@agsft.com>
2018-04-23 15:40:52 +0530

    add resume structure added

---
+++ RecruitmentManagementPortal/src/main/java/com/rmportal/controller/EmployeeReferalController.java
@@ -0,0 +1,13 @@
+package com.rmportal.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@RestController
+@Api(value="EmployeeReferal Controller", description="Refer the Candidate")
+@CrossOrigin("*")
+public class EmployeeReferalController {
+
+}
---
+++ RecruitmentManagementPortal/src/main/java/com/rmportal/repository/EmployeeReferalRepository.java
@@ -0,0 +1,9 @@
+package com.rmportal.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rmportal.model.EmployeeReferal;
+
+public interface EmployeeReferalRepository extends CrudRepository<EmployeeReferal, Integer>{
+
+}
