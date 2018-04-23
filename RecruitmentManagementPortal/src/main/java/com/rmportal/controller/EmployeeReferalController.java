d937d1064ce49031ca8e6530ee309395362324de
tejas kheng <tejaskhengare96@gmail.com>
2018-04-23 16:15:15 +0530

    emp controller

--- RecruitmentManagementPortal/src/main/java/com/rmportal/controller/EmployeeReferalController.java
+++ RecruitmentManagementPortal/src/main/java/com/rmportal/controller/EmployeeReferalController.java
@@ -1,6 +1,8 @@
 package com.rmportal.controller;
 
+import org.springframework.data.solr.core.RequestMethod;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
@@ -9,5 +11,7 @@ import io.swagger.annotations.Api;
 @Api(value="EmployeeReferal Controller", description="Refer the Candidate")
 @CrossOrigin("*")
 public class EmployeeReferalController {
+	
+	@RequestMapping(value="/getEmployeeDetails/{referal_id}" value=RequestMethod.)
 
 }
--- RecruitmentManagementPortal/src/main/java/com/rmportal/repository/EmployeeReferalRepository.java
+++ RecruitmentManagementPortal/src/main/java/com/rmportal/repository/EmployeeReferalRepository.java
@@ -1,9 +1,15 @@
 package com.rmportal.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.rmportal.model.EmployeeReferal;
+import com.rmportal.model.User;
 
 public interface EmployeeReferalRepository extends CrudRepository<EmployeeReferal, Integer>{
+	
+	@Query(value = "SELECT * FROM employee_referal WHERE referal_id =:referal_id", nativeQuery = true)
+	User findByEmployeeId(@Param("referal_id") int referalId);
 
 }
