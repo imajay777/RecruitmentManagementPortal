aca7a0c7c3f050287f2082f5f79c014a659f2027
Girish Muchalambe <girish@agsft.com>
2018-04-23 16:10:04 +0530

    employee referal response maodel and service

--- RecruitmentManagementPortal/src/main/java/com/rmportal/repository/EmployeeReferalRepository.java
+++ RecruitmentManagementPortal/src/main/java/com/rmportal/repository/EmployeeReferalRepository.java
@@ -1,9 +1,17 @@
 package com.rmportal.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.rmportal.model.EmployeeReferal;
+import com.rmportal.model.User;
 
+@Repository
 public interface EmployeeReferalRepository extends CrudRepository<EmployeeReferal, Integer>{
+	
+	@Query(value = "SELECT * FROM employee_referal WHERE referance_email =:referance_email", nativeQuery = true)
 
+	EmployeeReferal findByReferalEmail(@Param("referance_email")String referance_email);
 }
