8a430840a78a05708ef469f9e162b0206e6a1a3a
Sourabh J <jsourabh@agsft.com>
2018-05-11 12:24:07 +0530

    velocity template completed

--- RecruitmentManagementPortal/src/main/java/com/rmportal/RecruitmentManagementPortalApplication.java
+++ RecruitmentManagementPortal/src/main/java/com/rmportal/RecruitmentManagementPortalApplication.java
@@ -1,11 +1,16 @@
 package com.rmportal;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.exception.VelocityException;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.velocity.VelocityEngineFactory;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
@@ -15,7 +20,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableAutoConfiguration
 @SpringBootApplication
 @EnableSwagger2
 public class RecruitmentManagementPortalApplication {
@@ -48,5 +52,17 @@ public class RecruitmentManagementPortalApplication {
 	public Validator jsr303Validator() {
 		return new LocalValidatorFactoryBean();
 	}
-
+	
+	@Bean
+    public VelocityEngine getVelocityEngine() throws VelocityException, IOException{
+        VelocityEngineFactory factory = new VelocityEngineFactory();
+        Properties props = new Properties();
+        props.put("resource.loader", "classpath");
+        props.put("classpath.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
+        factory.setVelocityProperties(props);
+        
+        VelocityEngine ve = factory.createVelocityEngine();
+        ve.init();
+        return ve;      
+    }
 }
